57e954648b3174cc9b7eb4146d32da500dd75ad4
RobynSpaulding <robynm27@gmail.com>
2023-08-14 10:30:48 -0700

    child create action and route

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -4,4 +4,15 @@ class ChildrenController < ApplicationController
     @children = Child.all
     render :index 
   end
+
+  def create
+    @child = Child.create(
+      user_id: params[:user_id],
+      milestone_category: params[:milestone_category],
+      name: params[:name],
+      dob: params[:dob],
+      profile_image: params[:profile_image],
+    )
+    render :show
+  end
 end
