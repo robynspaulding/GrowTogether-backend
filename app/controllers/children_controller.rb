bd46c5129c59d8989f417e29c21a4184b05dfd41
RobynSpaulding <robynm27@gmail.com>
2023-08-25 13:59:41 -0700

    adds child_params method to child controller to make it more dry

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -7,12 +7,10 @@ class ChildrenController < ApplicationController
   end
 
   def create
-    @child = Child.create(
-      user_id: current_user.id,
-      name: params[:name],
-      dob: params[:dob],
-      profile_image: params[:profile_image],
-    )
+    @child = Child.new(child_params)
+    @child.user_id = current_user.id
+    
+    @child.save
     render :show
   end
 
@@ -24,7 +22,6 @@ class ChildrenController < ApplicationController
   def update
     @child = Child.find_by(id: params[:id])
     @child.update(
-      # user_id: params[:user_id] || @child.user_id,
       name: params[:name] || @child.name,
       dob: params[:dob] || @child.dob,
       profile_image: params[:profile_image] || @child.profile_image,
@@ -36,5 +33,9 @@ class ChildrenController < ApplicationController
     @child = Child.find_by(id: params[:id])
     @child.destroy
     render json: { message: "Child deleted successfully" }
+  end
+
+  def child_params
+    params.permit(:name, :dob, :profile_image)
   end
 end
